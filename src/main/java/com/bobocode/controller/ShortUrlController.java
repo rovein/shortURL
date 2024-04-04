4d3559acb31c48b4d0cbdc849dfb906d70f5ebc0
Roman Kuznetsov <a.roman.kuznetsov@gmail.com>
2024-04-04 08:39:52 +0300

    Create structure

--- src/main/java/com/bobocode/controller/ShortUrlController.java
+++ src/main/java/com/bobocode/controller/ShortUrlController.java
@@ -1,21 +1,37 @@
 package com.bobocode.controller;
 
+import com.bobocode.dto.CreateShortUrlRequest;
+import com.bobocode.service.ShortUrlService;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @Controller
+@AllArgsConstructor
 public class ShortUrlController {
 
-    // mvc
-    @PostMapping
-    public String createShortUrl() {
-        return "mvc-view";
-    }
+    private final ShortUrlService shortUrlService;
 
     @GetMapping
-    public void acceptShortUrl() {
+    public String index() {
+        return "index";
+    }
 
+    @PostMapping("/short-url")
+    public ResponseEntity<String> createShortUrl(@ModelAttribute("request") CreateShortUrlRequest request) {
+        return ResponseEntity.ok(shortUrlService.shortUrl(request.initialUrl()));
+    }
+
+    @GetMapping("/{url}") //localhost:8080/fwfv4D
+    public void acceptShortUrl(@PathVariable("url") String shortenedUrl, HttpServletResponse response) throws IOException {
+        var originalUrl = this.shortUrlService.getInitialUrl(shortenedUrl);
+        response.sendRedirect(originalUrl);
     }
 }
--- src/main/java/com/bobocode/service/ShortUrlService.java
+++ src/main/java/com/bobocode/service/ShortUrlService.java
@@ -1,12 +1,29 @@
 package com.bobocode.service;
 
+import org.springframework.stereotype.Service;
+import org.thymeleaf.util.StringUtils;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+@Service
 public class ShortUrlService {
 
-    public String shortUrl(String initialUrl) {
-        return "";
+    private final ConcurrentMap<String, String> shortToInitialUrl = new ConcurrentHashMap<>();
+
+    public String shortUrl(final String initialUrl) {
+        if (StringUtils.isEmpty(initialUrl)) {
+            throw new IllegalArgumentException("Provided initial url should not be empty!");
+        }
+        String shortenedUrl = StringUtils.randomAlphanumeric(6); //fwfv4D
+        shortToInitialUrl.put(shortenedUrl, initialUrl);
+        return shortenedUrl;
     }
 
-    public String getInitialUrl(String shortenedUrl) {
-        return "";
+    public String getInitialUrl(final String shortenedUrl) {
+        if (StringUtils.isEmpty(shortenedUrl)) {
+            throw new IllegalArgumentException("Provided shortened url should not be empty!");
+        }
+        return shortToInitialUrl.get(shortenedUrl);
     }
 }

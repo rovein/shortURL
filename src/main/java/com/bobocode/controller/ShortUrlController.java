213abf416679d6061efc83f6bbbce28420665ccb
Roman Kuznetsov <a.roman.kuznetsov@gmail.com>
2024-04-04 11:11:30 +0300

    Remove redundant imports

--- src/main/java/com/bobocode/controller/ShortUrlController.java
+++ src/main/java/com/bobocode/controller/ShortUrlController.java
@@ -4,8 +4,6 @@ import com.bobocode.dto.CreateShortUrlRequest;
 import com.bobocode.service.ShortUrlService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -14,7 +12,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.net.URI;
 

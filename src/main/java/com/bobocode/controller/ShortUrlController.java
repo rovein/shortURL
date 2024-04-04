bc89c5fe404f780c1e49eaf5eeaf23cf51944091
Roman Kuznetsov <a.roman.kuznetsov@gmail.com>
2024-04-04 11:11:00 +0300

    Integrate controller with view

--- src/main/java/com/bobocode/controller/ShortUrlController.java
+++ src/main/java/com/bobocode/controller/ShortUrlController.java
@@ -2,16 +2,21 @@ package com.bobocode.controller;
 
 import com.bobocode.dto.CreateShortUrlRequest;
 import com.bobocode.service.ShortUrlService;
-import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.io.IOException;
+import java.net.URI;
 
 @Controller
 @AllArgsConstructor
@@ -20,18 +25,30 @@ public class ShortUrlController {
     private final ShortUrlService shortUrlService;
 
     @GetMapping
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("request", new CreateShortUrlRequest(""));
         return "index";
     }
 
     @PostMapping("/short-url")
-    public ResponseEntity<String> createShortUrl(@ModelAttribute("request") CreateShortUrlRequest request) {
-        return ResponseEntity.ok(shortUrlService.shortUrl(request.initialUrl()));
+    public String createShortUrl(@ModelAttribute("request") CreateShortUrlRequest shortUrlRequest,
+                                 HttpServletRequest request,
+                                 Model model) {
+        String shortUrlId = shortUrlService.shortUrl(shortUrlRequest.initialUrl());
+        String requestUrl = request.getRequestURL().toString();
+        String url = requestUrl.substring(0, requestUrl.lastIndexOf("/")) + "/" + shortUrlId;
+        model.addAttribute("shortUrl", url);
+        return "index";
     }
 
-    @GetMapping("/{url}") //localhost:8080/fwfv4D
-    public void acceptShortUrl(@PathVariable("url") String shortenedUrl, HttpServletResponse response) throws IOException {
+    @GetMapping("/{url}")
+    public ResponseEntity<?> acceptShortUrl(@PathVariable("url") String shortenedUrl, HttpServletRequest request) {
         var originalUrl = this.shortUrlService.getInitialUrl(shortenedUrl);
-        response.sendRedirect(originalUrl);
+        if (!originalUrl.startsWith("http://")) {
+            originalUrl = "https://" + originalUrl;
+        }
+        return ResponseEntity.status(HttpStatusCode.valueOf(301))
+                .location(URI.create(originalUrl))
+                .build();
     }
 }
